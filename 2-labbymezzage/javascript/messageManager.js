bbbbfd6b6ba658b1f36ff8178e0fcc38fed197c1
OpenShift guest <aj222ah@student.lnu.se>
2013-12-03 06:06:27 -0500

    Moment 5

--- 2-labbymezzage/javascript/messageManager.js
+++ 2-labbymezzage/javascript/messageManager.js
@@ -1,12 +1,24 @@
 "use strict";
 
 function messageHandler () {
-    var sendButton = document.querySelector("sendButton");
+    var sendButton = document.getElementById("sendButton");
+    var messageHolder = document.getElementById("newMessage");
     var messages = [];
+    var createTime;
+    var userInput;
     
+    sendButton.addEventListener("click", createDate, false);
+    
+    function createDate(e) {
+        createTime = new Date();
+        userInput = messageHolder.value;
+        messages.push(new Message(userInput, createTime));
+    }
     
     
 }
+
+
 
 
 window.addEventListener("load", messageHandler, false);
