0090ae90cce2f17f49228984fd23ad859a50b429
OpenShift guest <aj222ah@student.lnu.se>
2013-12-03 04:52:14 -0500

    Moment 2 - lab 2

--- 2-labbymezzage/javascript/Message.js
+++ 2-labbymezzage/javascript/Message.js
@@ -1,5 +1,31 @@
 "use strict";
 
-function Message () {
+function Message (messageText, date) {
     
+    this.getText = function() {
+        return messageText;
+    };
+    
+    this.setText = function(textInput) {
+        messageText = textInput;
+    };
+    
+    this.getDate = function() {
+        return date;
+    };
+    
+    this.setDate = function(dateInput) {
+        date = dateInput;
+    };
+    
+}
+
+Message.prototype.toString = function() {
+    return this.getText() + " (" + this.getDate() + ").";
+};
+
+Message.prototype.getHTMLtext = function() {
+    var tempMessage = this.getText();
+    tempMessage.replace(/\n/g, "<br>");
+    return tempMessage;
 };
