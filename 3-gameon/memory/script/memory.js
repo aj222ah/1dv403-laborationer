b38cafa43863f6965bc9e2933e80a8a5ce6afe4d
OpenShift guest <aj222ah@student.lnu.se>
2013-12-11 11:21:59 -0500

    Uppdatering memory.js

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,8 +2,9 @@
 
 function Memory (rows, cols, placeHolder) {
     var bricks = [];
-    var pairs;
-    var rounds;
+    var pairs = 0;
+    var rounds = 0;
+    var that = this;
     
     this.getRows = function() {
         return rows;
@@ -89,19 +90,57 @@ Memory.prototype.play = function() {
 
 Memory.prototype.flip = function(e) {
     if (!e) { var e = window.event; }
-    var flipped = false;
-    var address;
-    var brickNo;
+    var brickArray = document.getElementsByClassName("brickAccessArray");
+    var imageLocation ="pics/0.png";
+    var flippedArray = [], i;
+    var address, index, classString, brickValues = [];
+    var brickNo, newAddress, noOfFlipped = 0, x = 0;
     
-    if(this.firstChild.className === flipped) {
-        address = this.firstChild.src;
-        alert(this.firstChild.src);
-        brickNo = function() {
-            var index = this.className.search(" ");
-            return this.className.splice(index);
-        };
-        alert(brickNo);
+    address = this.firstChild.getAttribute("src");
+    classString = this.getAttribute("class");
+    index  = classString.search(" ");
+    brickNo = classString.slice(index + 1);
+    newAddress = address.replace("0", brickNo);
+    this.firstChild.setAttribute("src", newAddress);
+    this.firstChild.setAttribute("class", "true");
+        
+    for(i = 0; i < brickArray.length; i++) {
+        flippedArray[i] = brickArray[i].firstChild.getAttribute("class");
+        if (flippedArray[i] === "true") {
+            noOfFlipped += 1;
+        }
     }
+    
+    alert(that.bricks.length);
+    if(noOfFlipped === 2) {
+        for(i = 0; i < flippedArray.length; i++) {
+            if (flippedArray[i] === true) {
+                brickValues[x] = Memory.bricks[i];
+            }
+        }
+        if (brickValues[0] === brickValues[1]) {
+            for(i = 0; i < flippedArray.length; i++) {
+                if (flippedArray[i] === true) {
+                    brickArray[i].removeEventListener("click", this.flip, false);
+                    brickArray[i].firstChild.setAttribute("class", "match");
+                }
+            }
+            Memory.pairs += 1;
+            Memory.rounds += 1;
+        }
+        else {
+            setTimeout(function() {
+                for(i = 0; i < flippedArray.length; i++) {
+                    if (flippedArray[i] === true) {
+                        brickArray[i].firstChild.setAttribute("src", imageLocation);
+                         brickArray[i].firstChild.setAttribute("class", "true");
+                         Memory.rounds += 1;
+                    }
+                }
+            }, 1000);
+        }
+    }
+    alert(Memory.rounds);
 };
 
 var MemoryApp = {
