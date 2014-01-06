3baadfe05965f641f0afb0247c12ff894721b6cb
OpenShift guest <aj222ah@student.lnu.se>
2014-01-06 12:07:50 -0500

    Modifierat utseendet på memoryt

--- 5-pwd/script/appManager.js
+++ 5-pwd/script/appManager.js
@@ -1,8 +1,51 @@
 "use strict";
 var ADAJAWM = ADAJAWM || {};
 
-ADAJAWM.Window = function() {
+ADAJAWM.WindowConstructor = function WindowConstructor(id, width, height, left, upper) {
+    
+    this.getId = function() {
+        return id;
+    };
+  
+    this.setId = function(ownId) {
+        id = ownId;
+    };
+    
+    this.getWidth = function() {
+        return width;
+    };
+  
+    this.setWidth = function(ownWidth) {
+        width = ownWidth;
+    };
+  
+    this.getHeigth = function() {
+        return height;
+    };
+  
+    this.setHeigth = function(ownHeight) {
+        height = ownHeight;
+    };
+  
+    this.getLeft = function() {
+        return left;
+    };
+  
+    this.setLeft = function(ownLeft) {
+        left = ownLeft;
+    };
+    
+    this.getUpper = function() {
+        return upper;
+    };
+  
+    this.setUpper = function(ownUpper) {
+        upper = ownUpper;
+    };
 };
+/*
+ADAJAWM.Window = function() {
+};*/
 
 ADAJAWM.ImageViewerWindow = function(e) {
     alert("Hej!");
